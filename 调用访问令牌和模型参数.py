afc50af28f1e6191d7beaf9f6dcbc03037f8f31a
runrun <155811667+runkoko@users.noreply.github.com>
2026-04-26 13:48:39 +0800

    Add script to retrieve OpenAI API credentials

---
+++ 调用访问令牌和模型参数.py
@@ -0,0 +1,6 @@
+import os
+OPENAI_BASE_URL = os.environ.get('OPENAI_BASE_URL')
+print(OPENAI_BASE_URL)
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+print(OPENAI_API_KEY)
+MODEL = "ernie-image-turbo"
